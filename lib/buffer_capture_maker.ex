7fa2cc298adbecbe328ecdad42de30ee10bbc940
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-02-04 21:51:13 +0100

    initial commit

---
+++ lib/buffer_capture_maker.ex
@@ -0,0 +1,34 @@
+defmodule Membrane.BufferCaptureMaker do
+  use Membrane.Sink
+
+  def_options location: [type: :string, description: "Path to save location"]
+
+  def_input_pad :input, demand_unit: :buffers, caps: :any
+
+  @impl true
+  def handle_init(options), do: {:ok, %{buffers: [], location: options.location}}
+
+  @impl true
+  def handle_caps(:input, _caps, _context, state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_prepared_to_playing(_ctx, state) do
+    {{:ok, demand: :input}, state}
+  end
+
+  @impl true
+  def handle_write(:input, buffer, _ctx, state) do
+    {{:ok, demand: :input}, %{state | buffers: [buffer | state.buffers]}}
+  end
+
+  @impl true
+  def handle_end_of_stream(:input, _ctx, state) do
+    {:ok, file} = File.open(state.location, [:write])
+    capture = Enum.reverse(state.buffers) |> :erlang.term_to_binary()
+    IO.binwrite(file, capture)
+
+    {:ok, state}
+  end
+end
